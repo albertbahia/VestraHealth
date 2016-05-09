4fb58845d7fd10913c3f35db33b9a4f7075846b6
albertbahia <albert.bahia1@gmail.com>
2016-05-09 00:35:56 -0400

    added handlebars and created appropriate folder structure accordingly

--- server.js
+++ server.js
@@ -1,14 +1,20 @@
 var express = require('express');
-var bodyParser = require('body-parser')
+var bodyParser = require('body-parser');
+var exphbs = require('express-handlebars');
 
 var app = express();
 
 // Define server port
 var PORT = process.env.PORT || 8080;
 
-// Middleware
-// Encode parsed data from forms and attach them a new `body` object via the req object in the routes 
+// ---Middleware---
+
+
+// Encode parsed data from forms and attach them a new `body` object via the req object in the routes
 app.use(bodyParser.urlencoded({extended: false}));
+// Serve static assets
+app.use(express.static(process.cwd() + '/public'));
+// -----End middleware------
 
 // Routes
 app.get('/', function(req, res) {
