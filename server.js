f2b60c7c15ebac1a8cc9075a62a644c903c11af0
albertbahia <albert.bahia1@gmail.com>
2016-05-08 23:18:42 -0400

    added body parser package

--- server.js
+++ server.js
@@ -1,1 +1,18 @@
 var express = require('express');
+var bodyParser = require('body-parser')
+
+var app = express();
+
+var PORT = process.env.PORT || 8080;
+
+
+// Routes
+app.get('/', function(req, res) {
+  if (err) throw err;
+  res.send('Hello World!');
+})
+
+// Listen to port
+app.listen(PORT, function() {
+  console.log("Listening on PORT: " + PORT);
+})
